566487330714e3c769e64916848c041811a51b17
shamildag <shamildag@gmail.com>
2023-03-28 10:51:42 +0300

    added new test just to check

--- tests/test_items.py
+++ tests/test_items.py
@@ -12,5 +12,23 @@ class MyTestCase(unittest.TestCase):
         df_temp2 = pd.DataFrame.from_dict(data2)
         join_DF_by_orderId(df_temp, df_temp2)
 
-        if __name__ == '__main__':
-            unittest.main()
+    def test_something2(self):
+        # creating the first DataFrame
+        df1 = pd.DataFrame({"fruit": ["apple", "banana", "avocado"],
+                            "market_price": [21, 14, 35]})
+        print("The first DataFrame")
+        print(df1)
+
+        # creating the second DataFrame
+        df2 = pd.DataFrame({"fruit": ["banana", "apple", "avocado"],
+                            "wholesaler_price": [65, 68, 75]})
+        print("The second DataFrame")
+        print(df2)
+
+        # joining the DataFrames
+        print("The merged DataFrame")
+        print(pd.merge(df1, df2, on="fruit", how="inner"))
+
+
+if __name__ == '__main__':
+    unittest.main()
